c3ebc69ac94fb688ff4875056fe6892997d204d8
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-01-26 21:28:03 +0530

    adding better auth prisma client

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,10 +1,10 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 export const auth = betterAuth({
-  // database: prismaAdapter(prisma, {
-  //   provider: "postgresql",
-  // }),
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
 });
