17657932301d850222f0d1e0aac7c11e36593e08
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-01-27 15:57:12 +0530

    Removed for next 15

--- server/index.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-import { cors } from "hono/cors";
-import { handle } from "hono/vercel";
-
-const app = new Hono().basePath("/api").use(cors())
-
-export const httpHandler = handle(app)
-
-export default app
