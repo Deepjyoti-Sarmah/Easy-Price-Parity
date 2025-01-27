ad1e017b746f6c2316ee3e36a90f0a69d1482b4e
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-01-27 17:07:38 +0530

    added hono for server

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { handle } from "hono/vercel";
+
+const app = new Hono().basePath("/api").use(cors())
+
+// const appRouter = app.route("/health", healthRouter)
+
+export const httpHandler = handle(app)
+
+export default app
