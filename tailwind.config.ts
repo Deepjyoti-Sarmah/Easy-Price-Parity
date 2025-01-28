1493c30f47723bb9d8173409512b62bdae48e9c3
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-01-28 20:43:36 +0530

    setup for landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,35 +1,18 @@
-import type { Config } from "tailwindcss"
-import { fontFamily } from "tailwindcss/defaultTheme"
+import type { Config } from "tailwindcss";
 
-const config: Config = {
+export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      fontFamily: {
-        heading: ["var(--font-heading)", ...fontFamily.sans],
-      },
       colors: {
-        brand: {
-          25: "#F7FAFD",
-          50: "#F0F5FA",
-          100: "#E1EBF5",
-          200: "#C7D7EB",
-          300: "#A9C3DE",
-          400: "#8BAECF",
-          500: "#6D99BF",
-          600: "#5483A3",
-          700: "#426A86",
-          800: "#335269",
-          900: "#273E4F",
-          950: "#1C2D3A",
-        },
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
   plugins: [],
-}
-export default config
+} satisfies Config;
