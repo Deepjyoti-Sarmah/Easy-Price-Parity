c41af28cf2b741dcfd697d0484f6a994ada82dac
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-01-29 15:49:35 +0530

    middleware setup

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,31 +1,19 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "./env";
 import { nextCookies } from "better-auth/next-js";
-import {
-  GITHUB_CLIENT_ID,
-  GITHUB_CLIENT_SECRET,
-  GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET
-} from "../env";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-  emailAndPassword: {
-    enabled: true
-  },
   socialProviders: {
     github: {
       clientId: GITHUB_CLIENT_ID,
       clientSecret: GITHUB_CLIENT_SECRET,
-    },
-    google: {
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET
-    },
+    }
   },
   plugins: [nextCookies()]
 });
--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,38 @@
 import { NextResponse, type NextRequest } from "next/server";
+import { Session } from "./lib/auth-types";
 import axios from "axios";
-import { Session } from "./lib/better-auth/auth-types";
+
 
 async function getMiddlewareSession(req: NextRequest) {
   const { data: session } = await axios.get<Session>("/api/auth/get-session", {
     baseURL: req.nextUrl.origin,
     headers: {
-      //get the cookie from the request
       cookie: req.headers.get("cookie") || "",
-    },
-  });
+    }
+  })
 
   return session;
 }
 
 export default async function authMiddleware(req: NextRequest) {
-  const session = await getMiddlewareSession(req);
-  if (!session) {
-    return NextResponse.redirect(new URL("/sign-in", req.url));
-  }
+  // const { data: session } = await betterFetch<Session>(
+  //   "/api/auth/get-session",
+  //   {
+  //     baseURL: request.nextUrl.origin,
+  //     headers: {
+  //       //get the cookie from the request
+  //       cookie: request.headers.get("cookie") || "",
+  //     },
+  //   },
+  // );
 
+  const session = await getMiddlewareSession(req);
+
+  // if (!session) {
+  //   return NextResponse.redirect(new URL("/sign-in", req.url));
+  // }
   return NextResponse.next();
 }
-
 
 export const config = {
   matcher: [
