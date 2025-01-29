cbccb5e4ca54397126deb74297b789d4082d83f7
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-01-29 16:40:39 +0530

    fixing auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "./env";
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "./env";
 import { nextCookies } from "better-auth/next-js";
 
 const prisma = new PrismaClient();
@@ -13,7 +13,11 @@ export const auth = betterAuth({
     github: {
       clientId: GITHUB_CLIENT_ID,
       clientSecret: GITHUB_CLIENT_SECRET,
-    }
+    },
+    google: {
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    },
   },
   plugins: [nextCookies()]
 });
