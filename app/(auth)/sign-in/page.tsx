ca7a268bb701f03a3570592193d42e4316b7d859
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-02-03 13:53:20 +0530

    adding sign in and sign up page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { useSearchParams } from "next/navigation"
+
+const SignInPage = () => {
+  const searchParams = useSearchParams()
+  const intent = searchParams.get("intent")
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <SignInForm intent={intent} />
+    </div>
+  )
+}
+
+export default SignInPage
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const SignUpPage = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <SignUpForm />
+    </div>
+  )
+}
+
+export default SignUpPage
