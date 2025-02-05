bec2b30ab7d184a9568999962abd9f564021aada
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-02-05 17:45:02 +0530

    fixing sign in page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link"
 
 export default function SignInPage() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-bun-background p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <Link href="/" className="flex items-center justify-center font-semibold text-zinc-300">
+    <div className="flex min-h-svh flex-col items-center justify-center bg-bun-background p-4 sm:p-6 md:p-8">
+      <div className="w-full max-w-sm">
+        <Link href="/" className="mb-8 block text-center text-2xl font-semibold text-zinc-300">
           Parity<span className="text-bunPurple-300/90">Cart</span>
         </Link>
         <LoginForm />
@@ -13,3 +13,4 @@ export default function SignInPage() {
     </div>
   )
 }
+
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,14 +1,15 @@
-"use client"
+import { LoginForm } from "../_components/login-form"
+import Link from "next/link"
 
-import { Heading } from "@/components/heading"
-
-
-const SignUpPage = () => {
+export default function SignUpPage() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <Heading>Sign Up</Heading>
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-bun-background p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link href="/" className="text-center font-semibold text-zinc-300">
+          Parity<span className="text-bunPurple-300/90">Cart</span>
+        </Link>
+        <LoginForm />
+      </div>
     </div>
   )
 }
-
-export default SignUpPage
