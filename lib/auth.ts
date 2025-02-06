a097b955981a311b6b0712cfaccf871f2870ca9e
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-02-06 20:08:31 +0530

    error with env

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "./env";
 import { nextCookies } from "better-auth/next-js";
 
 const prisma = new PrismaClient();
@@ -11,13 +10,13 @@ export const auth = betterAuth({
   }),
   socialProviders: {
     github: {
-      clientId: GITHUB_CLIENT_ID,
-      clientSecret: GITHUB_CLIENT_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
-    google: {
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-    },
+    // google: {
+    //   clientId: GOOGLE_CLIENT_ID,
+    //   clientSecret: GOOGLE_CLIENT_SECRET,
+    // },
   },
   plugins: [nextCookies()]
 });
