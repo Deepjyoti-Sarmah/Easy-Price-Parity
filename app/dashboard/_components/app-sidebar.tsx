2eaba0d6862d57df9ef68e9275527c8217147e21
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2025-02-09 02:36:27 +0530

    adding dashboard

--- app/dashboard/_components/app-sidebar.tsx
+++ app/dashboard/_components/app-sidebar.tsx
@@ -14,10 +14,6 @@ import {
   SquareTerminal,
 } from "lucide-react"
 
-import { NavMain } from "@/components/nav-main"
-import { NavProjects } from "@/components/nav-projects"
-import { NavUser } from "@/components/nav-user"
-import { TeamSwitcher } from "@/components/team-switcher"
 import {
   Sidebar,
   SidebarContent,
@@ -25,6 +21,11 @@ import {
   SidebarHeader,
   SidebarRail,
 } from "@/components/ui/sidebar"
+
+import { NavMain } from "./nav-main"
+import { NavProjects } from "./nav-projects"
+import { NavUser } from "./nav-user"
+import { TeamSwitcher } from "./team-switcher"
 
 // This is sample data.
 const data = {
